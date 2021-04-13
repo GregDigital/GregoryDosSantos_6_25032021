2e48453ab09ede3f174e3e4eac5dd8c0d68b656c
GregDigital <gdossantos.pro@gmail.com>
2021-04-13 19:20:32 +0200

    modif

--- Js/script.js
+++ Js/script.js
@@ -1,23 +1,21 @@
 let container = document.querySelector("#photographers");
 
 function generatePhotographer(user) {
-  console.log(user)
-    return `
+  console.log(user);
+  return `
     <article class="photographers_card">
                 <a href="">
                     <img class="portrait" src="./Photos/Photographers/${user.portrait}" alt="Photo du photographe MimiKeel">
                     <h2 class="name">${user.name}</h2> 
                 </a>
                 <p class="infos_photographer">
-                    <span class="city">${user.city}</span>
+                    <span class="city">${user.city}, ${user.country}</span>
                     <span class="tagline">${user.tagline}</span>
-                    <span class="price">${user.price}</span>
+                    <span class="price">${user.price} €</span>
                 </p>
                 <ul class="infos_photographer_tags">
-                    <li class=""><a href="index.html" class="filter" title="portrait">#Portrait</a></li>
-                    <li class=""><a href="index.html" class="filter" title="events">#Events</a></li>
-                    <li class=""><a href="index.html" class="filter" title="travel">#Travel</a></li>
-                    <li class=""><a href="index.html" class="filter" title="animals">#Animals</a></li>
+          
+                
                     
                 </ul>
             </article>`;
@@ -25,17 +23,28 @@ function generatePhotographer(user) {
 
 function show(response) {
   let acc = [];
-  
+
+
+
   for (let photographer of response.photographers) {
     acc.push(generatePhotographer(photographer));
   }
 
-  
-
   let html = acc.reduce((a, l) => a + l);
   container.innerHTML = html;
+
+
+
+
 }
+
+
+
+
+
 
 fetch("json/profil.json")
   .then((response) => response.json())
   .then((json) => show(json));
+
+
