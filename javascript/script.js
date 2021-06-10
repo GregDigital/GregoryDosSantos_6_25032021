ad53deda518b7325417db8d301126ffca0cdb92b
GregDigital <gdossantos.pro@gmail.com>
2021-06-10 18:18:39 +0200

    modif

--- javascript/script.js
+++ javascript/script.js
@@ -2,57 +2,39 @@
 
 let container = document.querySelector("#photographers");
 
-
 function initFilters() {
+  const containerTaggs = document.querySelector(".navTagg");
+  const containerCards = document.querySelectorAll(".photographers_card");
 
-
-    //selecting all required elements
-    const filterItem = document.querySelectorAll(".tags");
-    const filterImg = document.querySelectorAll(".photographers_card");
-
-   
-      filterItem.onclick = (selectedItem)=>{ //if user click on filterItem div
-        if(selectedItem.target.classList.contains("tags")){ //if user selected item has .item class
-          filterItem.querySelector(".active").classList.remove("active"); //remove the active class which is in first item
-          selectedItem.target.classList.add("active"); //add that active class on user selected item
-          let filterName = selectedItem.target.getAttribute("data-filter"); //getting data-name value of user selected item and store in a filtername variable
-          filterImg.forEach((image) => {
-            let filterImges = image.getAttribute("data-filter"); //getting image data-name value
-            //if user selected item data-name value is equal to images data-name value
-            //or user selected item data-name value is equal to "all"
-            if(filterImges == filterName){
-              image.classList.remove("hide"); //first remove the hide class from the image
-              image.classList.add("show"); //add show class in image
-            }else{
-              image.classList.add("hide"); //add hide class in image
-              image.classList.remove("show"); //remove show class from the image
-            }
-          });
-        }
-      }
-
+  containerTaggs.addEventListener("click", (e) => {
+    console.log(e.target);
+    //if (e.target.classList.add("tags")){
+    //containerTaggs.querySelector(".active").classList.to("active")
+    e.target.classList.toggle("active");
+    const filterDataNav = e.target.getAttribute("data-filter");
     
-     
-
+  
+    //}
+  });
 }
 
 function generatePhotographerTags(tags) {
-    let acc = [];
-    for (let tag of tags) {
-        acc.push(`<li class="filter">#${tag}</li>`)
-    }
-    let html = acc.reduce((a, l) => a + l);
-    return html
-    
+  let acc = [];
+  for (let tag of tags) {
+    acc.push(`<li class="filter">#${tag}</li>`);
+  }
+  let html = acc.reduce((a, l) => a + l);
+  return html;
 }
 
 function generatePhotographer(user) {
-
-    console.log(user.id);
-    return `
-    <article class="photographers_card" data-filter="${user.tags}">
+  console.log(user.id);
+  return `
+    <article class="photographers_card" data-filters="${user.tags}">
                 <a class="test" href="photographer-page.html?id=${user.id}">
-                    <img class="portrait" src="./Photos/Photographers/${user.portrait}" alt="Photo du photographe MimiKeel">
+                    <img class="portrait" src="./Photos/Photographers/${
+                      user.portrait
+                    }" alt="Photo du photographe MimiKeel">
                     <h2 class="name">${user.name}</h2>
                 </a>
                 <p class="infos_photographer">
@@ -64,29 +46,17 @@ function generatePhotographer(user) {
                 ${generatePhotographerTags(user.tags)}
                 </ul>
             </article>`;
-         
-
 }
-
-
-
-
-
-
-
-
-
-
 
 function show(response) {
-    let acc = [];
-    for (let photographer of response.photographers) {
-        acc.push(generatePhotographer(photographer));
-    }
-    let html = acc.reduce((a, l) => a + l);
-    container.innerHTML = html;
-    initFilters(response.photographers)
+  let acc = [];
+  for (let photographer of response.photographers) {
+    acc.push(generatePhotographer(photographer));
+  }
+  let html = acc.reduce((a, l) => a + l);
+  container.innerHTML = html;
+  initFilters(response.photographers);
 }
 fetch("json/profil.json")
-    .then((response) => response.json())
-    .then((json) => show(json));
+  .then((response) => response.json())
+  .then((json) => show(json));
